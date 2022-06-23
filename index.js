f72b51351fa0eee77d9b5c548da72b84bd0fa1d1
Kevin <magavakevin10@gmail.com>
2022-06-23 16:01:23 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+//removes the element with ID main
+document.getElementById('main').remove()
+
+//creates element h1 and assigns it newHeader
+const newHeader = document.createElement("h1")
+
+//sets text
+newHeader.textContent = "YOUR-NAME is the champion"
+
+//sets ID
+newHeader.id = "victory"
